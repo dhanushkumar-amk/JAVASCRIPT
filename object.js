1e7c7c4fd114a6644b6db0f9fcdf00a6b57482f8
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-03-07 07:29:53 +0530

    day four  of learning javaSript

---
+++ object.js
@@ -0,0 +1,34 @@
+// object contains the key : values,// syntax:// var person: {//     id: number;//     name: string;
+//     age: number;
+//     status: boolean;
+// avengers: () => void;
+// }
+
+var person = {
+  id: 1,
+  name: 'dhanushkumar',
+  age: 17,
+  status: true,
+};
+
+// another method
+// old method
+// used only on the passing the variable inside the object
+// var person1 = Object.create({
+//   id: 1,
+//   name: 'dhanushkumar',
+//   age: 17,
+//   status: true,
+// });
+
+// person.name;
+//first method
+// console.log(person.name);
+// dhanushkumar
+
+// second method
+// console.log(person['name']);
+
+// dhanushkumar
+
+console.log();
