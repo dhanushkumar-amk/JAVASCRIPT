d7dd39b3059d311065bac5ebdc9722f78abc9067
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-02 10:16:12 +0530

    modified

---
+++ src/Topics/Arrays/find.js
@@ -0,0 +1,11 @@
+/** Arrays.prototype.filter() */
+/** Returns the value of the first element in the array where predicate is true, and undefined otherwise. */
+
+/** always return first value of the that condition is stratified */
+const fruits = ['apple', 'orange', 'mongo', 'banana'];
+
+const result = fruits.find((num) => {
+  return fruits.sort();
+});
+
+console.log(result);
