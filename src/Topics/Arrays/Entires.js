0a0025eb333bb03cc51540f79b62160475788f18
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-02 08:45:23 +0530

    modified

---
+++ src/Topics/Arrays/Entires.js
@@ -0,0 +1,7 @@
+/** Arrays.prototype.entries() */
+/** Returns an iterable of key, value pairs for every entry in the array */
+
+const fruits = ['apple', 'orange', 'mongo', 'banana'];
+const fruit = fruits.entries();
+
+console.log(fruit);
---
+++ src/Topics/Arrays/ForEach.js
@@ -0,0 +1,15 @@
+//Arrays.prototype.forEach
+
+//Performs the specified action for each element in an array.
+
+let Arrays = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+Arrays.forEach((Array) => {
+  console.log(Array * 10);
+});
+
+let fruits = ['apple', 'mongo'];
+
+fruits.forEach((fruit) => {
+  console.log(fruit.toUpperCase());
+});
