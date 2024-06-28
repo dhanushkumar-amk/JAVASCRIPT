ce6118ec2b296522aec4402da35f7699e0055a5f
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-28 20:59:57 +0530

    modified

--- OBJECT/Prototype.js
+++ OBJECT/Prototype.js
@@ -3,6 +3,7 @@ const user = {
     greet() {
         console.log(`My name is ${this.name}`);
     }
+    
 }
 // prototype is a unbuild function in object
 // console.log(Object.prototype.toString(user));
